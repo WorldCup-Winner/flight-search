6e08851f26c5160c00f1a50fa5488ee53b726576
WorldCup-Winner <worldcupwinner216@gmail.com>
2025-09-16 21:19:15 -0400

    fix again

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -51,7 +51,7 @@
   </nav>
 </template>
 
-<script setup lang="ts">
+<script setup lang="js">
 import { ref } from 'vue'
 import SignIn from "@/components/auth/SignIn.vue"
 import SignUp from "@/components/auth/SignUp.vue"
