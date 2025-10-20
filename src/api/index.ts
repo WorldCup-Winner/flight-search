edf391b17784fb0e0d971b7662508024f659c077
WorldCup-Winner <worldcupwinner216@gmail.com>
2025-10-19 21:06:09 -0400

    add: base url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,37 +1,41 @@
 import axios from 'axios'
 
+const api = axios.create({
+    baseURL: import.meta.env.API_BASE_URL,
+});
+
 export function getAirlines() {
-    return axios.get('https://galilee-flight-backend.pamis.dev/api/airline')
+    return api.get('https://galilee-flight-backend.pamis.dev/api/airline')
 }
 
 export function getLocations() {
-    return axios.get('https://galilee-flight-backend.pamis.dev/api/location')
+    return api.get('https://galilee-flight-backend.pamis.dev/api/location')
 }
 
 export function flightSearch(req: any) {
-    return axios.post('https://galilee-flight-backend.pamis.dev/api/flight-search', req)
+    return api.post('https://galilee-flight-backend.pamis.dev/api/flight-search', req)
 }
 
 export function signIn(req: any) {
-    return axios.post('https://galilee-flight-backend.pamis.dev/api/Liyi/FA01', req)
+    return api.post('https://galilee-flight-backend.pamis.dev/api/Liyi/FA01', req)
 }
 
 export function sendSMS(req: any) {
-    return axios.post('https://galilee-flight-backend.pamis.dev/api/Liyi/FA01B', req)
+    return api.post('https://galilee-flight-backend.pamis.dev/api/Liyi/FA01B', req)
 }
 
 export function getAirlineAlliance() {
-    return axios.get('https://galilee-flight-backend.pamis.dev/api/airline-alliance/airlines')
+    return api.get('https://galilee-flight-backend.pamis.dev/api/airline-alliance/airlines')
 }
 
 export function getFareRule(req: any) {
-    return axios.post('https://galilee-flight-backend.pamis.dev/api/fare-rule', req)
+    return api.post('https://galilee-flight-backend.pamis.dev/api/fare-rule', req)
 }
 
 export function booking(req: any) {
-    return axios.post('https://galilee-flight-backend.pamis.dev/api/booking', req)
+    return api.post('https://galilee-flight-backend.pamis.dev/api/booking', req)
 }
 
 export function LiyiFP04(req: any) {
-    return axios.post('https://galilee-flight-backend.pamis.dev/api/Liyi/FP04', req)
+    return api.post('https://galilee-flight-backend.pamis.dev/api/Liyi/FP04', req)
 }
